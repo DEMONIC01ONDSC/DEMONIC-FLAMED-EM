980991afc56142f9128b29015bf099e7e30d38bb
slurza iskool <>
2022-10-08 21:50:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,127 @@
+import os, discord
+from discord.ext import commands
+ 
+token = input("big boy token:")
+client = commands.Bot(command_prefix=commands.when_mentioned_or("$"))
+os.system('cls')
+os.system(f'press bot')
+os.system(f'mode 120,40')
+
+@client.event
+async def on_ready():
+  print(f""" \u001b[31m
+MADE BY DEMONIC
+#1 J4J/AUTO PRESSURE
+------------------------------
+""")
+
+
+
+@client.event
+async def on_message(message):
+    channel = message.channel
+    if message.content.endswith('SHUT UP NIGGA'):
+    
+        await message.channel.send ('on gdk boa yo ahh tweakin on chillam onnem foe wrd2 my deadz')
+        await message.channel.send ('ya got me fucked up wrd2 lamrod nd dem on gbg yo ahh tweakin foe ')
+        await message.channel.send ('I AM UR ELDERLORD NIGGA')
+        await message.channel.send ('YOU THINK YOU STAND A CHANCE FOOLISH MORTAL?')
+        await message.channel.send ('*MANIACAL LAUGH*')
+        await message.channel.send ('BITCH ASS NIGGA THINKS HE CAN HANDLE ME')
+        await message.channel.send ('I GOT AN OPP EXTERMINATOR ON MY SIDE NIGGA')
+        await message.channel.send ('UR GRANNY PACKING DIRT RN LOLLOL')
+        await message.channel.send ('U LICK FRUITY PEBBLES OFF THE FLOOR FATASS NIGGA')
+        await message.channel.send ('WANT ANOTHER HAMBURGER??? :hamburger: :hamburger: :hamburger: ')
+        await message.channel.send ('FATASS NIGGA')
+        await message.channel.send ('THIS IS U TYPING RN https://thumbs.dreamstime.com/b/fat-teenager-boy-eat-junk-food-play-computer-games-lead-unhealthy-passive-lifestyle-bedroom-195639592.jpg')
+        await message.channel.send ('GETTING HANDCRAMPS ALREADY NIGGA?')
+        await message.channel.send ('🔪🔪🔪🔪🔪')
+        await message.channel.send ('WANNABE YEAT EDGY EMO ASS NIGGA https://thefader-res.cloudinary.com/private_images/c_limit,w_1024/c_crop,h_533,w_1024,x_0,y_26,f_auto,q_auto:eco/Yeat_37_duw9zd/Yeat_37_duw9zd.jpg')
+        await message.channel.send ('ARABIAN ASS TACO MUNCHER SPEAKING 2 ME')
+        await message.channel.send ('YA MOM GOT VAULTED FOR HAVING ESEX WITH MARSKI NIGGA LOL')
+        await message.channel.send ('END IT ALL ON FACEBOOK LIVE EMO ASS NIGGA')
+        await message.channel.send ('LLFOLAOALOLOLOOL')
+        await message.channel.send ('IM FLAMING YOU LOL DUCKING ME PUSSY')
+        await message.channel.send ('HOW MANY WALMART GOODS CAN I SCAN ON UR WRIST')
+        await message.channel.send ('DONT GHOST CHAT NOW')
+        await message.channel.send ('"I FELL IN LOVE WITH AN EMO GIRL" FACEASS NIGGA https://ddbuckingham.files.wordpress.com/2017/09/large.png')
+        await message.channel.send ('UR MY FUCKING JR CLOWN ASS NEGROID 🤡')
+        await message.channel.send ('LIL LAME ASS MUNCHKIN')
+        await message.channel.send ('UR UNCLE GOT RAN OFF CORD FOR BEING A PEDO')
+        await message.channel.send ('UR A GAY FURRY FEMBOY MY NIGGA')
+        await message.channel.send ('U LIKE DICK IN UR ASS SON LOL')
+        await message.channel.send ('GO WATCH RIPCORD TUTORIAL THEN TRY TO PACK ME NIGGA UR SO ASS')
+        await message.channel.send ('DONT TRY TO RUN NOW')
+        await message.channel.send ('UR MY GOON NOW LMFAO')
+        await message.channel.send ('HES SCARED RN LMFAO')
+        await message.channel.send ('WHY U SHAKING ')
+        await message.channel.send ('L')
+        await message.channel.send ('O')
+        await message.channel.send ('L')
+        await message.channel.send ('NIGGA IS QUIVERING')
+        await message.channel.send ('IM')
+        await message.channel.send ('UR')
+        await message.channel.send ('GOD')
+        await message.channel.send ('SONNY ASS NIGGA ')
+        await message.channel.send ('LO')
+        await message.channel.send ('O')
+        await message.channel.send ('L')
+        await message.channel.send ('SON STOP SLEEPING AND FOCUS ON ME https://thumbs.dreamstime.com/z/joyful-guy-sleeping-teddy-bear-childish-peacefully-comfortable-bed-74050303.jpg')
+        await message.channel.send ('NIGGA TRIED TO PRESSURE ME')
+        await message.channel.send ('UR A FUCKING DOWNIE SON')
+        await message.channel.send ('FOUND YOU! https://raisingchildren.net.au/__data/assets/image/0028/47674/autism-spectrum-disorder-sensory-sensitivitiesnarrow.jpg')
+        await message.channel.send ('STOP CRYING LOL')
+        await message.channel.send ('ITS OK TO LOSE ')
+        await message.channel.send ('UR')
+        await message.channel.send ('MY')
+        await message.channel.send ('JR')
+        await message.channel.send ('NOW')
+        await message.channel.send ('OL')
+        await message.channel.send ('L')
+        await message.channel.send ('OLO')
+        await message.channel.send ('LO')
+        await message.channel.send ('ITS WRAPS FOR U NIGGA')
+        await message.channel.send ('I CAUGHT U FUCKING A RINO IN 4K LISTENING TO DRAKE')
+        await message.channel.send ('STOP FOLDING SO QUICK')
+        await message.channel.send ('SPEECHLESS NIGGA')
+        await message.channel.send ('U SUCK')
+        await message.channel.send ('LOLLLOOLOLOLOLL')
+        await message.channel.send ('ILL RUN U OUT OF COM PUSSY BOY')
+        await message.channel.send ('THIS WEAK BITCH LMFAO')
+        await message.channel.send ('LAME ASS BOY NEEDS HIS REST')
+        await message.channel.send ('LMAO')
+        await message.channel.send ('FOCUS ON PRESSURING AND PACKING WEAK BOY CANT MULTI TASK')
+        await message.channel.send ('MY SON IS GETTING TIRED')
+        await message.channel.send ('MY GRANDPA TYPES FASTER')
+        await message.channel.send ('SIT DOWN SON')
+        await message.channel.send ('DONT TRY LOLLL')
+        await message.channel.send ('YOU"RE NOTHING')
+        await message.channel.send ('KEEP CRYING')
+        await message.channel.send ('CRY MORE')
+        await message.channel.send ('CRY BABY')
+        await message.channel.send ('Im surprised you didnt leave yet :skull:')
+        await message.channel.send ('DID U GET WORSE??????')
+        await message.channel.send ('SONNY BOY')
+        await message.channel.send ('JUST TOOK YO SOUL NIGGA!!!!!')
+        await message.channel.send ('NIGGA UR LEGIT ASS')
+        await message.channel.send ('COME GET SONNED NIGGA')
+        await message.channel.send ('U SUCK SON')
+        await message.channel.send ('UR TRAPPED HERE NIGGA')
+        await message.channel.send ('SHUT UP')
+        await message.channel.send ('SIT YO ASS DOWN NIGGA')
+        await message.channel.send ('DEMONIC ON TOP NIGGA CRY ABT IT FAGGOT')
+        await message.channel.send ('DONT EVER TRY TO STEP TO ME AGAIN')
+        await message.channel.send ('SHUT MY DICK NIGGA')
+
+
+
+
+
+
+
+
+
+
+
+
+client.run(token, bot=False)
